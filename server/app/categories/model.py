64e1a87f6271857225945dcc868f048530c0b92d
Caiwan <caiwan.lupus@gmail.com>
2019-02-05 10:40:37 +0100

    Category model fix

--- server/app/categories/model.py
+++ server/app/categories/model.py
@@ -1,13 +1,13 @@
 import peewee
+
 from app import components
 
 
 class Category(components.BaseDocumentModel):
-    title = peewee.TextField()
+    name = peewee.TextField()
+    comment = peewee.TextField(default="")
     is_archived = peewee.BooleanField(default=False)
     is_protected = peewee.BooleanField(default=False)
     order = peewee.IntegerField(default=0)
-    spanning_order = peewee.IntegerField(default=0)
+    flatten_order = peewee.IntegerField(default=0)
     parent = peewee.ForeignKeyField("self", backref="children", null=True)
-    # ... TBD
-    # decoration = ? like colors, meta etc
