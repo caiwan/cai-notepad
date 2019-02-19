c105917d4d2ac7ac3709edc76786888cf6f6cdde
Caiwan <caiwan.lupus@gmail.com>
2019-02-19 15:34:27 +0100

    Skip unimplemented use-cases from tests

--- server/app/tests/test_endpoint.py
+++ server/app/tests/test_endpoint.py
@@ -16,8 +16,17 @@ class MyAttr(dict):
     pass
 
 
-def attr(path, method, payload=None, params=None, as_user=None, expected_status=[200, 201]):
-    r = MyAttr({"path": path, "method": method, "payload": payload, "params": params, "as_user": as_user, "expected_status": expected_status})
+def attr(path, method,
+         payload=None,
+         params=None,
+         as_user=None,
+         expected_status=[200, 201],
+         skip=False,
+         skip_reason="Temporary disabled"
+         ):
+    #
+    r = MyAttr({"path": path, "method": method, "payload": payload, "params": params, "as_user": as_user,
+                "expected_status": expected_status, "skip": skip, "skip_reason": skip_reason})
     setattr(r, "__name__", "%s_%s_as_%s" % (method, path.replace("/", "_"), as_user if as_user else "anonymous"))
     return r
 
@@ -31,27 +40,27 @@ class TestEndpointAccess(TestUtils, TestCase):
 
     def setUp(self):
         self._setup_app()
-        # + add Note
-        # + add Task
-        # + add Category
+        # TODO + add test Note
+        # TODO + add test Task
+        # TODO + add test Category
         pass
 
     @ddt.data(
         # auth
-        attr("/auth/register/", "post", payload={}),
+        attr("/auth/register/", "post", payload={}, skip=True, skip_reason="Endpoint not implemented"),
         attr("/auth/login/", "post", payload={"username": "user", "password": "password"}),
         attr("/auth/logout/", "post", as_user="user"),
         attr("/auth/renew/", "post", as_user="user", payload={}),
-        attr("/auth/password_reset/", "get", as_user="user", payload={}),
-        attr("/auth/profile/", "get", as_user="user"),
-        attr("/auth/profile/", "put", as_user="user", payload={}),
+        attr("/auth/password_reset/", "get", as_user="user", payload={}, skip=True, skip_reason="Endpoint not implemented"),
+        attr("/auth/profile/", "get", as_user="user", skip=True, skip_reason="Endpoint not implemented"),
+        attr("/auth/profile/", "put", as_user="user", payload={}, skip=True, skip_reason="Endpoint not implemented"),
 
         # -- anonymous access
         attr("/auth/logout/", "post", expected_status=[403]),
         attr("/auth/renew/", "post", expected_status=[403]),
-        attr("/auth/password_reset/", "post", expected_status=[403]),
-        attr("/auth/profile/", "get", expected_status=[403]),
-        attr("/auth/profile/", "put", expected_status=[403], payload={}),
+        attr("/auth/password_reset/", "post", expected_status=[403], skip=True, skip_reason="Endpoint not implemented"),
+        attr("/auth/profile/", "get", expected_status=[403], skip=True, skip_reason="Endpoint not implemented"),
+        attr("/auth/profile/", "put", expected_status=[403], payload={}, skip=True, skip_reason="Endpoint not implemented"),
 
         # user
         attr("/users/", "get", as_user="admin"),
@@ -63,35 +72,39 @@ class TestEndpointAccess(TestUtils, TestCase):
         # notes
         attr("/notes/", "get", as_user="user"),
         attr("/notes/", "post", as_user="user", payload={"title": "Note", "content": "Content", "tags": []}),
-        attr("/notes/1/", "get", as_user="user"),
-        attr("/notes/1/", "put", as_user="user", payload={"title": "Note", "content": "Content", "tags": []}),
-        attr("/notes/1/", "delete", as_user="user"),
+        attr("/notes/1/", "get", as_user="user", skip=True, skip_reason="Test data required"),
+        attr("/notes/1/", "put", as_user="user", payload={"title": "Note",
+                                                          "content": "Content", "tags": []}, skip=True, skip_reason="Test data required"),
+        attr("/notes/1/", "delete", as_user="user", skip=True, skip_reason="Test data required"),
 
         # tasks
         attr("/tasks/", "get", as_user="user"),
         attr("/tasks/", "post", as_user="user", payload={"title": "Task"}),
-        attr("/tasks/1/", "get", as_user="user"),
-        attr("/tasks/1/", "put", as_user="user", payload={"title": "Task"}),
-        attr("/tasks/1/", "delete", as_user="user"),
+        attr("/tasks/1/", "get", as_user="user", skip=True, skip_reason="Test data required"),
+        attr("/tasks/1/", "put", as_user="user", payload={"title": "Task"}, skip=True, skip_reason="Test data required"),
+        attr("/tasks/1/", "delete", as_user="user", skip=True, skip_reason="Test data required"),
 
         # --- tasks as anonymous
         attr("/tasks/", "get", expected_status=[403]),
         attr("/tasks/", "post", expected_status=[403], payload={}),
-        attr("/tasks/1/", "get", expected_status=[403]),
-        attr("/tasks/1/", "put", expected_status=[403], payload={}),
-        attr("/tasks/1/", "delete", expected_status=[403]),
+        attr("/tasks/1/", "get", expected_status=[403], skip=True, skip_reason="Test data required"),
+        attr("/tasks/1/", "put", expected_status=[403], payload={}, skip=True, skip_reason="Test data required"),
+        attr("/tasks/1/", "delete", expected_status=[403], skip=True, skip_reason="Test data required"),
 
         # categories
         attr("/categories/", "get", as_user="user"),
         attr("/categories/", "post", as_user="user", payload={"name": "Category", "parent": None}),
-        attr("/categories/1/", "get", as_user="user"),
-        attr("/categories/1/", "put", as_user="user", payload={"name": "Category", "parent": None}),
-        attr("/categories/1/", "delete", as_user="user"),
+        attr("/categories/1/", "get", as_user="user", skip=True, skip_reason="Test data required"),
+        attr("/categories/1/", "put", as_user="user", payload={"name": "Category", "parent": None}, skip=True, skip_reason="Test data required"),
+        attr("/categories/1/", "delete", as_user="user", skip=True, skip_reason="Test data required"),
 
         # tag autosearch
         attr("/tags/autocomplete/", "get", params={"q": ""}, as_user="user")
     )
     def test_endpoint(self, attr):
+        if attr["skip"]:
+            self.skipTest(attr["skip_reason"])
+            return
 
         methods = {
             "get": self.app.get,
--- server/app/user/tests/test_user.py
+++ server/app/user/tests/test_user.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-from unittest import TestCase
+from unittest import TestCase, skip
 import json
 
 from app import components
@@ -27,10 +27,12 @@ class TestUser(TestUtils, TestCase):
     def tearDown(self):
         self._db.close()
 
+    skip("Registration is not implemented yet")
     def test_register(self):
         # given no user
         # when register a new user
         # then registration shall be succeed
+        self.fail("Registration is not implemented yet")
         pass
 
     def test_login(self):
@@ -56,19 +58,25 @@ class TestUser(TestUtils, TestCase):
 
         pass
 
+    @skip("Token security check is not implemented yet")
     def test_token(self):
         # TODO
         # given user with login and token
         # when requesting a protected resource
         # then a resource should be accessed without error
+        self.fail("Token security check is not implemented yet")
+
         pass
 
+    @skip("Permission security is not implemented yet")
     def test_permissions(self):
         # TODO
+        self.fail("Permission security is not implemented yet")
         pass
 
     def test_logout(self):
-        # given user with login and token
+        # given
+        # - user with login and token
         credentials = {
             "username": self.REGULAR_USER,
             "password": self.REGULAR_PW
@@ -78,12 +86,14 @@ class TestUser(TestUtils, TestCase):
         self.assertTrue("token" in response_json, msg="no token was given")
         token_id = response_json["token"]
 
-        # when logout
+        # when
+        # - logout
         response = self.app.get(self.LOGOUT, **self.post_args, headers={
             "Authorization": "Bearer %s" % token_id
         })
 
-        # then an access token shall be removed
+        # then
+        # - an access token shall be removed
         try:
             token = Token.get(Token.token_id == token_id)
             self.fail("No token ws deleted, still active %s" % token.token_id)
