2f82d3f1da11df81512fd53052b4ee4fe27228e2
Caiwan <caiwan.lupus@gmail.com>
2019-02-25 16:07:03 +0100

    WIP User profile + 3rd party connections

---
+++ client/src/components/user/component.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="main container-flex">
+    <div class="col-md-12">
+      <header class="row">
+        User profile goez here
+      </header>
+    </div>
+
+  </section>
+</template>
+<script />
+<style />
